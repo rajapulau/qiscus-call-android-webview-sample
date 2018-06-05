8ea1f598c29711d310d744946d55ff3766f0278d
Fitra Aditya <fitra@g.pl>
2018-06-05 07:50:48 +0700

    Add conf webview activity.

---
+++ app/src/main/java/com/qiscus/rtc/webviewsample/conference/ConfActivity.java
@@ -0,0 +1,106 @@
+package com.qiscus.rtc.webviewsample.conference;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+import android.webkit.CookieManager;
+import android.webkit.JavascriptInterface;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.qiscus.rtc.webviewsample.MainActivity;
+import com.qiscus.rtc.webviewsample.R;
+import com.qiscus.sdk.Qiscus;
+
+public class ConfActivity extends AppCompatActivity {
+    private String[] permissions = {
+            Manifest.permission.CAMERA,
+            Manifest.permission.INTERNET,
+            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+            Manifest.permission.RECORD_AUDIO
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conf);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestPermission(permissions);
+
+        Bundle extras = getIntent().getExtras();
+        String call_room_id = extras.getString("call_room_id");
+
+        JavaScriptInterface javaScriptInterface = new JavaScriptInterface(this);
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.addJavascriptInterface(javaScriptInterface, "AndroidFunction");
+        setUpWebViewDefaults(webView);
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {;
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+        });
+        webView.loadUrl("file:///android_asset/room.html#" + call_room_id);
+    }
+
+    private void setUpWebViewDefaults(WebView webView) {
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDisplayZoomControls(false);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+        settings.setMediaPlaybackRequiresUserGesture(false);
+        webView.setWebContentsDebuggingEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url){
+                webView.loadUrl("javascript:setName('" + Qiscus.getQiscusAccount().getEmail() + "')");
+            }
+        });
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptThirdPartyCookies(webView, true);
+    }
+
+    public void requestPermission(String[] requestedPermission) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, requestedPermission, 101);
+        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            //
+        }
+    }
+
+    public class JavaScriptInterface {
+        Context context;
+
+        JavaScriptInterface(Context c) {
+            context = c;
+        }
+
+        @JavascriptInterface
+        public void endCall() {
+            Log.d("SINI", "HALO");
+            startActivity(new Intent(context, MainActivity.class));
+            finish();
+        }
+    }
+}
